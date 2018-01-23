7523afc875409ecc1bff33fdeeab7fcf41f2f470
masaibar <masaibar.dev@gmail.com>
2018-01-23 13:10:49 +0900

    Toastで"hoge"と出すだけのサンプルを追加

--- app/src/main/java/com/masaibar/daggersample/MainActivity.kt
+++ app/src/main/java/com/masaibar/daggersample/MainActivity.kt
@@ -2,11 +2,21 @@ package com.masaibar.daggersample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            showDebugText("hoge")
+        }
+    }
+
+    private fun showDebugText(text: String) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
     }
 }
